cc45748137fc39ec42d19992899400e26132c3c1
tonny0831 <tonnyjansen0831@gmail.com>
2022-07-12 18:14:12 +0900

    Plus addresses

--- src/components/Content/Content.js
+++ src/components/Content/Content.js
@@ -13,6 +13,20 @@ const fliTokenDetails = [
     strategy_extension_address: ["0xe6484a64e2ea165943c734dC498070b5902CBc2b"],
   },
   {
+    product: "ETH2x-FLI",
+    token_address: "0xaa6e8127831c9de45ae56bb1b0d4d4da6e5665bd",
+    keeper_address: "0xEa80829C827f1633A46E7EA6026Ed693cA54eebD",
+    manage_address: "0x0749CE17c983Deb806D6b841c65b7359C3d5b104 ",
+    strategy_extension_address: ["0x9bA41A2C5175d502eA52Ff9A666f8a4fc00C00A1", "0xe833C90F4d07650aC1d8a915C2c0fdDBEDC1ec3A"],
+  },
+  {
+    product: "BTC2x-FLI",
+    token_address: "0x0b498ff89709d3838a063f1dfa463091f9801c2b",
+    keeper_address: "0xCBEb906f46eA0b9D7e7e75379fAFbceACd1aAeff",
+    manage_address: "0x3b439351177fC9d7b5fd11aEdCc177D73F989341 ",
+    strategy_extension_address: ["0xFD4eA597E8346a6723FA4A06a31E4b6F7F37e9Ad", "0x5e568E47C52E8af9dB47e52D0Ae7c740f8f85BF1"],
+  },
+  {
     product: "BTC2x-FLI-P",
     token_address: "0xd6ca869a4ec9ed2c7e618062cdc45306d8dbbc14",
     keeper_address: "0xB0d01F7e741C7F708386bE1438A70eDBA9B29a86",
@@ -45,25 +59,19 @@ const fliTokenDetails = [
   },
   {
     product: "ETH2x-FLI-P",
-    token_address: "0xaa6e8127831c9de45ae56bb1b0d4d4da6e5665bd",
-    keeper_address: "0xEa80829C827f1633A46E7EA6026Ed693cA54eebD",
-    manage_address: "0x0749CE17c983Deb806D6b841c65b7359C3d5b104",
-    strategy_extension_address: ["0x9ba41a2c5175d502ea52ff9a666f8a4fc00c00a1", "0xe833C90F4d07650aC1d8a915C2c0fdDBEDC1ec3A"],
+    token_address: "0x3ad707da309f3845cd602059901e39c4dcd66473",
+    keeper_address: "0x9Edf6539124ce7E82767a695eCB313B5a584c14A",
+    manage_address: "0x2622c4BB67992356B3826b5034bB2C7e949ab12B",
+    strategy_extension_address: ["0xb97F5a34696adf30db822612379235c3c53B714A", "0xe6484a64e2ea165943c734dC498070b5902CBc2b"],
   },
   {
-    product: "ETH2x-FLI",
-    token_address: "0xaa6e8127831c9de45ae56bb1b0d4d4da6e5665bd",
-    keeper_address: "0xEa80829C827f1633A46E7EA6026Ed693cA54eebD",
-    manage_address: "0x0749CE17c983Deb806D6b841c65b7359C3d5b104",
-    strategy_extension_address: ["0x9ba41a2c5175d502ea52ff9a666f8a4fc00c00a1", "0xe833C90F4d07650aC1d8a915C2c0fdDBEDC1ec3A"],
+    product: "MATIC2x-FLI-P",
+    token_address: "0xf287d97b6345bad3d88856b26fb7c0ab3f2c7976",
+    keeper_address: "0x00C68B75e1A8ddF7680e694b0E441FD62C87Cba2",
+    manage_address: "0x0bd75d6efe664991cab603d140375b2f4f70dd70",
+    strategy_extension_address: ["0x31459836b4F19c84eC004b92aef6b00f7490da52", "0xEa381e5b73cd416e577a36a498b3Fa78DD21dF5F"],
   },
-  {
-    product: "BTC2x-FLI",
-    token_address: "0x0b498ff89709d3838a063f1dfa463091f9801c2b",
-    keeper_address: "0xCBEb906f46eA0b9D7e7e75379fAFbceACd1aAeff",
-    manage_address: "0x3b439351177fC9d7b5fd11aEdCc177D73F989341",
-    strategy_extension_address: ["0xFD4eA597E8346a6723FA4A06a31E4b6F7F37e9Ad", "0x5e568E47C52E8af9dB47e52D0Ae7c740f8f85BF1"],
-  }
+
 ];
 
 export default function Content() {
@@ -100,6 +108,84 @@ export default function Content() {
     let index = 0;
     fliTokenDetails.map(async (item) => {
       if (item.product === "icETH") {
+        const bal = await web3Eth.eth.getBalance(item.keeper_address);
+        item.strategy_extension_address.map(async (extension_address) => {
+          try {
+            const eth_instance = new web3Eth.eth.Contract(
+              eth_ABI,
+              extension_address
+            );
+            const leverage_data = await eth_instance.methods
+              .getMethodology()
+              .call();
+            result.push({
+              product: item.product,
+              token_address: item.token_address,
+              chain: "Mainnet",
+              keeper_wallet: item.keeper_address,
+              wallet_balance: (bal / 10 ** 18).toFixed(4) + " ETH",
+              manage_address: item.manage_address,
+              strategy_extension_address: extension_address,
+              current_leverage_ratio:
+                leverage_data.recenteringSpeed / 10 ** 18 + "x",
+              target_leverage_ratio:
+                leverage_data.targetLeverageRatio / 10 ** 18 + "x",
+              min_leverage_ratio:
+                leverage_data.minLeverageRatio / 10 ** 18 + "x",
+              max_leverage_ratio:
+                leverage_data.maxLeverageRatio / 10 ** 18 + "x",
+            });
+
+            index++;
+            if (index === fliTokenDetails.length) {
+              setData(result);
+            }
+          } catch (e) {
+            
+          }
+        });
+
+      } 
+      if (item.product === "ETH2x-FLI") {
+        const bal = await web3Eth.eth.getBalance(item.keeper_address);
+        item.strategy_extension_address.map(async (extension_address) => {
+          try {
+            const eth_instance = new web3Eth.eth.Contract(
+              eth_ABI,
+              extension_address
+            );
+            const leverage_data = await eth_instance.methods
+              .getMethodology()
+              .call();
+            result.push({
+              product: item.product,
+              token_address: item.token_address,
+              chain: "Mainnet",
+              keeper_wallet: item.keeper_address,
+              wallet_balance: (bal / 10 ** 18).toFixed(4) + " ETH",
+              manage_address: item.manage_address,
+              strategy_extension_address: extension_address,
+              current_leverage_ratio:
+                leverage_data.recenteringSpeed / 10 ** 18 + "x",
+              target_leverage_ratio:
+                leverage_data.targetLeverageRatio / 10 ** 18 + "x",
+              min_leverage_ratio:
+                leverage_data.minLeverageRatio / 10 ** 18 + "x",
+              max_leverage_ratio:
+                leverage_data.maxLeverageRatio / 10 ** 18 + "x",
+            });
+
+            index++;
+            if (index === fliTokenDetails.length) {
+              setData(result);
+            }
+          } catch (e) {
+            
+          }
+        });
+
+      } 
+      if (item.product === "BTC2x-FLI") {
         const bal = await web3Eth.eth.getBalance(item.keeper_address);
         item.strategy_extension_address.map(async (extension_address) => {
           try {
@@ -285,81 +371,44 @@ export default function Content() {
         });
         
       }
-      if (item.product === "ETH2x-FLI") {
-        const bal = await web3Eth.eth.getBalance(item.keeper_address);
-
-        item.strategy_extension_address.map(async (extension_address) => {
-          try {
-            const pol_instance = new web3Eth.eth.Contract(
-              eth_ABI,
-              extension_address
-            );
-            const leverage_data = await pol_instance.methods
-              .getMethodology()
-              .call();
-            result.push({
-              product: item.product,
-              token_address: item.token_address,
-              chain: "Polygon",
-              keeper_wallet: item.keeper_address,
-              wallet_balance: (bal / 10 ** 18).toFixed(4) + " MATIC",
-              manage_address: item.manage_address,
-              strategy_extension_address: extension_address,
-              current_leverage_ratio:
-                leverage_data.recenteringSpeed / 10 ** 18 + "x",
-              target_leverage_ratio:
-                leverage_data.targetLeverageRatio / 10 ** 18 + "x",
-              min_leverage_ratio:
-                leverage_data.minLeverageRatio / 10 ** 18 + "x",
-              max_leverage_ratio:
-                leverage_data.maxLeverageRatio / 10 ** 18 + "x",
-            });
-            index++;
-            if (index === fliTokenDetails.length) {
-              setData(result);
-            }
-          } catch (e) {}
-        });
-        
-      }
-      if (item.product === "BTC2x-FLI") {
-        const bal = await web3Eth.eth.getBalance(item.keeper_address);
-
-        item.strategy_extension_address.map(async (extension_address) => {
-          try {
-            const pol_instance = new web3Eth.eth.Contract(
-              eth_ABI,
-              extension_address
-            );
-            const leverage_data = await pol_instance.methods
-              .getMethodology()
-              .call();
-            result.push({
-              product: item.product,
-              token_address: item.token_address,
-              chain: "Polygon",
-              keeper_wallet: item.keeper_address,
-              wallet_balance: (bal / 10 ** 18).toFixed(4) + " MATIC",
-              manage_address: item.manage_address,
-              strategy_extension_address: extension_address,
-              current_leverage_ratio:
-                leverage_data.recenteringSpeed / 10 ** 18 + "x",
-              target_leverage_ratio:
-                leverage_data.targetLeverageRatio / 10 ** 18 + "x",
-              min_leverage_ratio:
-                leverage_data.minLeverageRatio / 10 ** 18 + "x",
-              max_leverage_ratio:
-                leverage_data.maxLeverageRatio / 10 ** 18 + "x",
-            });
-            index++;
-            if (index === fliTokenDetails.length) {
-              setData(result);
-            }
-          } catch (e) {}
-        });
-        
-      }
       if (item.product === "ETH2x-FLI-P") {
+        const bal = await web3Polygon.eth.getBalance(item.keeper_address);
+
+        item.strategy_extension_address.map(async (extension_address) => {
+          try {
+            const pol_instance = new web3Polygon.eth.Contract(
+              eth_ABI,
+              extension_address
+            );
+            const leverage_data = await pol_instance.methods
+              .getMethodology()
+              .call();
+            result.push({
+              product: item.product,
+              token_address: item.token_address,
+              chain: "Polygon",
+              keeper_wallet: item.keeper_address,
+              wallet_balance: (bal / 10 ** 18).toFixed(4) + " MATIC",
+              manage_address: item.manage_address,
+              strategy_extension_address: extension_address,
+              current_leverage_ratio:
+                leverage_data.recenteringSpeed / 10 ** 18 + "x",
+              target_leverage_ratio:
+                leverage_data.targetLeverageRatio / 10 ** 18 + "x",
+              min_leverage_ratio:
+                leverage_data.minLeverageRatio / 10 ** 18 + "x",
+              max_leverage_ratio:
+                leverage_data.maxLeverageRatio / 10 ** 18 + "x",
+            });
+            index++;
+            if (index === fliTokenDetails.length) {
+              setData(result);
+            }
+          } catch (e) {}
+        });
+        
+      }
+      if (item.product === "MATIC2x-FLI-P") {
         const bal = await web3Polygon.eth.getBalance(item.keeper_address);
 
         item.strategy_extension_address.map(async (extension_address) => {
